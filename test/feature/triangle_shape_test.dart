f61030561562107ec06bcae22baa1e8b86939af1
Fatih Wolkk <ahmdfatihin17@gmail.com>
2023-06-15 09:49:28 +0700

    feat: check equilateral triangle

--- test/feature/triangle_shape_test.dart
+++ test/feature/triangle_shape_test.dart
@@ -3,14 +3,28 @@ import 'package:flutter_test/flutter_test.dart';
 void main() {
   //If one side is < 1, then it fails.
   detectTriangle(num sideA, num sideB, num sideC) {
-    if (sideA < 1 || sideB < 1 || sideC < 1) {
-      throw Exception();
+    final sides = [sideA, sideB, sideC];
+
+    for (var side in sides) {
+      if (side < 1) {
+        throw Exception();
+      }
+    }
+
+    //Check equilateral triangle
+    if (sideA == sideB && sideA == sideC && sideB == sideC) {
+      return "This is equilateral triangle";
     }
   }
 
   group('Detect the triangle', () {
     test('Should throw Error when there is side less than 1', () {
       expect(() => detectTriangle(-1, 2, 2), throwsA(isA<Exception>()));
+    });
+
+    test('Should return "equilateral triangle" when all sides are equal', () {
+      expect(detectTriangle(1, 1, 1), "equilateral triangle");
+      expect(detectTriangle(1, 1, 2), isNot("equilateral triangle"));
     });
   });
 }
