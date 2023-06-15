c59470df063e92695d9824e2bed25b7da06d0bb4
Fatih Wolkk <ahmdfatihin17@gmail.com>
2023-06-15 09:33:12 +0700

    feat: detect triangle side

--- test/feature/triangle_shape_test.dart
+++ test/feature/triangle_shape_test.dart
@@ -1,6 +1,13 @@
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
+  //If one side is < 1, then it fails.
+  detectTriangle(num sideA, num sideB, num sideC) {
+    if (sideA < 1 || sideB < 1 || sideC < 1) {
+      throw Exception();
+    }
+  }
+
   group('Detect the triangle', () {
     test('Should throw Error when there is side less than 1', () {
       expect(() => detectTriangle(-1, 2, 2), throwsA(isA<Exception>()));
